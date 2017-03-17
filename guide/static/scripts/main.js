1c2635df7659dbc37c2d5bbcb822d03507ac921d
Andrei Melis <andrei.melis@eaudeweb.ro>
2017-03-17 14:54:36 +0200

    Testing main.js

---
+++ guide/static/scripts/main.js
@@ -0,0 +1,10 @@
+(function() {
+
+  var sidebarTabs = new Tabs({
+    elem: 'sidebar-tabs',
+    titleClass: 'sidebar-tab-menu-item',
+    activeClass: 'active',
+    contentClass: 'sidebar-tab-pane'
+  });
+
+}())
